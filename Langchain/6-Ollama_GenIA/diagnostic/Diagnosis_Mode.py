ce2f8d00086c6e1b26f2cfe8f2589ac98b06f878
Baruch David <kaproma@yahoo.de>
2025-12-17 13:38:23 +0100

    add diagnosis modes

---
+++ Langchain/6-Ollama_GenIA/diagnostic/Diagnosis_Mode.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class DiagnosisMode(Enum):
+    ROLES_ONLY = ("rules_only",)
+    LLM_ONLY = ("llm_only",)
+    RULES_THEN_LLM = ("rules_then_llm",)
+    RULES_AND_LLM = "rules_and_llm"
