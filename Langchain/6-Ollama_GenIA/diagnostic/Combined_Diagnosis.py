c216ca50ce098f060f565cb4cd2d3d33f5938b6a
Baruch David <kaproma@yahoo.de>
2025-12-19 14:10:39 +0100

    old file pathes removing

--- Langchain/6-Ollama_GenIA/diagnostic/Combined_Diagnosis.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from .llm_Diagnosis import LlmDiagnosis
-
-
-class CombinedDiagnosis(BaseModel):
-    rule_based: LlmDiagnosis | None
-    llm_based: LlmDiagnosis
-    final: LlmDiagnosis
--- Langchain/6-Ollama_GenIA/diagnostic/Diagnosis_Mode.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class DiagnosisMode(Enum):
-    RULES_ONLY = "rules_only"
-    LLM_ONLY = "llm_only"
-    RULES_THEN_LLM = "rules_then_llm"
-    RULES_AND_LLM = "rules_and_llm"
