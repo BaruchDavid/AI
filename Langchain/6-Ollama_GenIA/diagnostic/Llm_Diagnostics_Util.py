b91ff86a2f5ca5c5f96cd728088cf3fdef8f8734
Baruch David <kaproma@yahoo.de>
2025-12-19 11:58:48 +0100

    remove unused imports

--- Langchain/6-Ollama_GenIA/diagnostic/Llm_Diagnostics_Util.py
+++ Langchain/6-Ollama_GenIA/diagnostic/Llm_Diagnostics_Util.py
@@ -1,4 +1,4 @@
-from typing import Any, Mapping, Optional
+from typing import Optional
 from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import SystemMessage, HumanMessage
 from .diagnosis_Mode import DiagnosisMode
