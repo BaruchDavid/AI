64400ed1971c84c3d233121804c62e40bdaee626
Baruch David <kaproma@yahoo.de>
2026-07-23 22:49:26 +0200

    setup graph and call result displaying

--- Langchain/15-ChatBot/src/langgraph_agentic_ai/main.py
+++ Langchain/15-ChatBot/src/langgraph_agentic_ai/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from src.langgraph_agentic_ai.ui.load_ui import LoadStreamlitUI
-
+from src.langgraph_agentic_ai.LLMs.groq_llm import GroqLLM
+from src.langgraph_agentic_ai.graph.graph_builder import GraphBuilder
+from src.langgraph_agentic_ai.ui.streamlitui.display_result import DisplayResultSteamlit
 
 def load_langgraph_agentic_ai_app():
     """
@@ -19,3 +21,42 @@ def load_langgraph_agentic_ai_app():
         return
 
     user_message = st.text_input("Enter your message:", key="user_message")
+
+    if user_message:
+
+        try:
+            ## Configure LLM model based on user selection
+            groqLLM = GroqLLM(user_controls_input=user_input)
+            model = groqLLM.get_llm_models()
+
+            if not model:
+                st.error("Error: failed to initialize the LLM model.")
+                return
+
+            ## Initialize and set up the graph based on use case
+            usecase = user_input.get("selected_usecase")
+            
+            if not usecase:
+                st.error("Error: No use case selected.")
+                return
+            
+            ## Graph Builder 
+            graph_builder = GraphBuilder(model)
+            try:    
+                
+                graph = graph_builder.setup_graph(usecase)
+            
+            except Exception as e:
+                st.error(f"Error setting up the graph: {e}")
+                return
+            
+            ## Display the result in Streamlit UI
+            display_result = DisplayResultSteamlit(usecase, graph, user_message)
+            display_result.display_result()
+            
+            
+
+        except Exception as e:
+            st.error(f"Error processing input: {e}")
+            if st.session_state.get("messages"):
+                st.session_state["messages"][-1]["content"] = "Error processing your message."
