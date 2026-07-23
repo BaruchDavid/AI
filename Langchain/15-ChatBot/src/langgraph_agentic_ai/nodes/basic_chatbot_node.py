5386b186bc0b718988327ba4817edf9c70abf261
Baruch David <kaproma@yahoo.de>
2026-07-23 22:51:04 +0200

    fix bug, in line 12 llm-attribute gonna bie called and not model

--- Langchain/15-ChatBot/src/langgraph_agentic_ai/nodes/basic_chatbot_node.py
+++ Langchain/15-ChatBot/src/langgraph_agentic_ai/nodes/basic_chatbot_node.py
@@ -1,11 +1,11 @@
+from src.langgraph_agentic_ai.state.state import State
 
-from src.langgraph_agentic_ai.state import State
 
 class BasicChatbotNode:
     def __init__(self, model):
-        self.model = model
+        self.llm = model
 
-    def process(self, state:State) -> dict:
+    def process(self, state: State) -> dict:
         """
         Processes the input text using the provided LLM and returns the response.
         """
