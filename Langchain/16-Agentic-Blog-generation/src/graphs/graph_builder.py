7bfe5e677c8691d789769ceac902bc4f0e3c792b
Baruch David <kaproma@yahoo.de>
2026-07-30 11:39:33 +0200

    build graphs with implemented node

---
+++ Langchain/16-Agentic-Blog-generation/src/graphs/graph_builder.py
@@ -0,0 +1,28 @@
+from langgraph.graph import StateGraph, START, END
+from src.llms.groqllm import GroqLLM
+from src.states.blogstate import BlogState
+from src.nodes.blog_node import BlogNode
+
+
+class GraphBuilder:
+    def __init__(self, llm):
+        self.llm = llm
+        self.graph = StateGraph(BlogState)
+
+    def build_topic_graph(self):
+        """
+        Build a graph to generate blogs based on topic
+        """
+
+        blog_node = BlogNode(self.llm)
+
+        # Add nodes to the graph
+        self.graph.add_node("title_creation", blog_node.title_creation)
+        self.graph.add_node("content_generation", blog_node.content_generation)
+
+        # Add edges to the graph
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", END)
+
+        return self.graph
