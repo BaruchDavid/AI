a601f6021d45203c1d14b12bd809ce4c3619670a
Baruch David <kaproma@yahoo.de>
2026-08-09 11:19:00 +0200

    get llm from env

--- Langchain/16-Agentic-Blog-generation/src/llms/groqllm.py
+++ Langchain/16-Agentic-Blog-generation/src/llms/groqllm.py
@@ -1,4 +1,4 @@
-from langchain_groq import GroqLLM
+from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 
@@ -8,8 +8,8 @@ class GroqLLM:
     def __init__(self):
         load_dotenv()
         self.api_key = os.getenv("GROQ_API_KEY")
-        self.model_name = os.getenv("GROQ_MODEL_NAME", "groq-llm-1.0")
-        self.client = GroqLLM(api_key=self.api_key, model_name=self.model_name)
+        self.groq_model = os.getenv("GROQ_MODEL")
+        self.client = ChatGroq(api_key=self.api_key, model_name=self.groq_model)
 
     def get_llm(self):
         return self.client
