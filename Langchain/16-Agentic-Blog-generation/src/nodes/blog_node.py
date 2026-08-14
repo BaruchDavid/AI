810ac3bad262fa59804b4053560ae0df359e34fb
Baruch David <kaproma@yahoo.de>
2026-08-14 10:35:11 +0200

    add node with prompt for llm to translate

--- Langchain/16-Agentic-Blog-generation/src/nodes/blog_node.py
+++ Langchain/16-Agentic-Blog-generation/src/nodes/blog_node.py
@@ -1,4 +1,6 @@
 from src.states.blogstate import BlogState
+from langchain_core.messages import SystemMessage, HumanMessage
+from src.states.blogstate import Blog
 
 
 class BlogNode:
@@ -40,3 +42,37 @@ class BlogNode:
             system_message = prompt.format(topic=state["topic"])
             response = self.llm.invoke(system_message)
             return {"blog": {"title": state["blog"]["title"], "content": response.content}}
+
+    def translation(self, state: BlogState) -> str:
+        """
+        Translate the blog content to the specified language
+        """
+        blog_content = state["blog"]["content"]
+        language = state["current_language"]
+
+        prompt = f"""
+
+                You are an expert translator. Use Markdown formatting. Translate the following blog content to {language}
+                - Maintain the original tone, style, and formatting of the content.
+                - Ensure that the translated content is culturally appropriate and relevant for the target audience.
+                - Adapt cultural references, idioms, and examples to resonate with the target audience.
+
+                ORIGINAL CONTENT:
+                {blog_content}
+
+                """
+
+        response = self.llm.invoke(prompt)
+        return {"blog": {"title": state["blog"]["title"], "content": response.content}}
+
+    def route(self, state: BlogState):
+        """set current language, which will be passed by lambda function to the translation node"""
+        return {"current_language": state["current_language"]}
+
+    def route_decision(self, state: BlogState):
+        if state["current_language"] == "german":
+            return "german"
+        elif state["current_language"] == "french":
+            return "french"
+        else:
+            return state["current_language"]
