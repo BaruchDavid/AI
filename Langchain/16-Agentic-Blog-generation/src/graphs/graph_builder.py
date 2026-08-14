b6b4d81b4bb4f5d09eb956653b8c3579d4f32e62
Baruch David <kaproma@yahoo.de>
2026-08-14 10:35:47 +0200

    add graph to execute languages logik

--- Langchain/16-Agentic-Blog-generation/src/graphs/graph_builder.py
+++ Langchain/16-Agentic-Blog-generation/src/graphs/graph_builder.py
@@ -27,12 +27,50 @@ class GraphBuilder:
 
         return self.graph
 
+    def build_graph_with_language(self):
+        """
+        Build a graph to generate blogs based on language
+        """
+
+        blog_node = BlogNode(self.llm)
+
+        # Add nodes to the graph
+        self.graph.add_node("title_creation", blog_node.title_creation)
+        self.graph.add_node("content_generation", blog_node.content_generation)
+        ## lege als key-value paar fest, dass 'current_langugage' german ist
+        self.graph.add_node("translate_to_german", lambda state: blog_node.translation({**state, "current_language": "german"}))
+        ## lege als key-value paar fest, dass 'current_langugage' german ist
+        self.graph.add_node("translate_to_french", lambda state: blog_node.translation({**state, "current_language": "french"}))
+
+        self.graph.add_node("route", blog_node.route)
+
+        # Add edges and conditional edges
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+
+        # decide, which node should be executed based on the current_language in the state
+        ## source: der Knoten, von dem die bedingte Kante ausgeht (bei dir: "route")
+        ## path: die Funktion, die zur Laufzeit entscheidet, wohin es geht (bei dir: blog_node.route_decision)
+        ## path_map: das Dict, das den Rückgabewert von path auf den tatsächlichen Zielknoten abbildet
+        self.graph.add_conditional_edges(
+                                            source="route",
+                                            path=blog_node.route_decision,
+                                            path_map={"german": "translate_to_german", "french": "translate_to_french"},
+                                        )
+        self.graph.add_edge("translate_to_german", END)
+        self.graph.add_edge("translate_to_french", END)
+
+        return self.graph
+
     def setup_graph(self, usecase: str):
         """
         Setup the graph with the initial state
         """
         if usecase == "topic":
             self.build_topic_graph()
+        if usecase == "language":
+            self.build_graph_with_language()
 
         return self.graph.compile()
 
